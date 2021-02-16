de8cd3ff8b3d2a9e5d98b8ad5f871f8c97b05fd2
hiden52 <zerorequiem23@gmail.com>
2021-02-16 14:59:32 +0000

    main.js 코드 작성중 .... for..of... 작동안한다. 확인 ㄱ

--- JS_GameProject/shopping-template/src/main.js
+++ JS_GameProject/shopping-template/src/main.js
@@ -1,1 +1,39 @@
-'use strict'
+"use strict";
+
+const loadItems = function () {
+    return fetch("data/clothes.JSON")
+        .then(response => response.json())
+        .then(json => console.log(json));
+}
+
+function displayItems(itmes) {
+    const itemUl = document.querySelector(".items");
+    
+    //이거 왜ㅑ 작동 안하지?? 확인해보자
+    for(const clothes of items.items) {
+        const item = document.createElement("li");
+        item.className = "item";
+
+        const img = document.createElement("img");
+        img.src = `img/${clothes.color}_${clothes.type.charAt(0)}.png`;
+        img.alt = clothes.type;
+        img.className = "item_thumbnail";
+
+        const span = document.createElement("span").innerText = `${clothes.gender}, ${clothes.size}`;
+        span.className = "item_description";
+
+        item.appendChild(img);
+        item.appendChild(span);
+        console.log(item);
+
+        itemUl.appendChild(item);
+    }
+}
+
+//main
+loadItems()
+    .then((items) => {
+        displayItems(items);
+        //setEventListener(items);
+    })
+    .catch(console.log);
