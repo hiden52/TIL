eb5ed2fe8104603dcc293e70204e51789d5ed3cb
hiden52 <zerorequiem23@gmail.com>
2021-02-17 11:38:41 +0000

    please add function "filtering"

--- JS_GameProject/shopping-template/src/main.js
+++ JS_GameProject/shopping-template/src/main.js
@@ -2,38 +2,77 @@
 
 const loadItems = function () {
     return fetch("data/clothes.JSON")
-        .then(response => response.json())
-        .then(json => console.log(json));
+        .then((response) => response.json())
+        .then((json) => json.items);
+    //.then(json => console.log(json); 하면 promise 리턴이 안돼서 에러!
+};
+
+function createLiInHTML(item) {
+    return `
+        <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item_thumbnail" />
+            <span class="item_description">${item.gender}, ${item.size}</sapn>
+        </li>
+    `;
+}
+function displayItems_(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createLiInHTML(item)).join('');
 }
 
-function displayItems(itmes) {
+// 이 함수는 각 li를 생성해서 ul에 child로 붙이기 때문에 refresh가 힘듦
+function createLiFromItem(itemOfData) {
+    const li = document.createElement("li");
+    li.className = "item";
+
+    const img = document.createElement("img");
+    img.src = `img/${itemOfData.color}_${itemOfData.type.charAt(0)}.png`;
+    img.alt = itemOfData.type;
+    img.className = "item_thumbnail";
+
+    const span = document.createElement("span");
+    span.className = "item_description";
+    span.innerText = `${itemOfData.gender}, ${itemOfData.size}`;
+
+    li.appendChild(img);
+    li.appendChild(span);
+
+    return li;
+}
+
+function displayItems(items) {
     const itemUl = document.querySelector(".items");
-    
-    //이거 왜ㅑ 작동 안하지?? 확인해보자
-    for(const clothes of items.items) {
-        const item = document.createElement("li");
-        item.className = "item";
 
-        const img = document.createElement("img");
-        img.src = `img/${clothes.color}_${clothes.type.charAt(0)}.png`;
-        img.alt = clothes.type;
-        img.className = "item_thumbnail";
-
-        const span = document.createElement("span").innerText = `${clothes.gender}, ${clothes.size}`;
-        span.className = "item_description";
-
-        item.appendChild(img);
-        item.appendChild(span);
-        console.log(item);
-
-        itemUl.appendChild(item);
+    // 작동함 ㅇㅋ
+    for (const clothes of items) {
+        itemUl.appendChild(createLiFromItem(clothes));
     }
+}
+
+function filterByColor(items, colorForFilter) {
+    const result = items.filter(item => item.color === colorForFilter);
+    return result;
+}
+function fillterByType(itmes, typeForFilter) {
+    const result = items.filter(item => item.type === typeForFilter);
+    return result;
+}
+
+function setEventListener(items) {
+    const logo =document.querySelector(".logo");
+    const btn = document.querySelector(".blue");
+    logo.addEventListener("click", () => displayItems_(items));
+    btn.addEventListener("click", function(){
+        displayItems_(filterByColor(items, "blue"));
+    });
+
 }
 
 //main
 loadItems()
-    .then((items) => {
-        displayItems(items);
-        //setEventListener(items);
+    .then((data) => {
+        // displayItems(data); // refresh 힘듦
+        displayItems_(data);
+        setEventListener(data);
     })
     .catch(console.log);
